9a5858fb9f0de4b9d5f55800e4ddee9cee23d0ef
Joseph <pumphrey.joseph@gmail.com>
2019-01-11 11:12:54 -0800

    players can choose their name

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab04_TicTacToe.Classes;
 
 namespace Lab04_TicTacToe
 {
@@ -6,7 +7,20 @@ namespace Lab04_TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player p1 = new Player();
+            Player p2 = new Player();
+            //player once picks their name
+            Console.Write("Player one write your name: ");
+            string userInput1 = Console.ReadLine();
+            userInput1 = p1.Name;
+
+            // player two picks their name
+            Console.Write("Player Two write your name: ");
+            string userInput2 = Console.ReadLine();
+            userInput2 = p2.Name;
+            // init new game
+            Game game = new Game(p1, p2);
+            game.Play();
         }
     }
 }
